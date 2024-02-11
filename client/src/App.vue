f1278c9136a07164eff70d81df4f9b93b95d4365
DiAbello <ipotapov101@gmail.com>
2024-02-12 00:54:23 +0400

    Initial commit

---
+++ client/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="wrapper">
+
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+.wrapper {
+  background-color: var(--primary-black);
+  min-height: 100%;
+}
+</style>
