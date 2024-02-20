9bb49b4a2b2fa9fa711e395394c9d3b87cbf905b
DiAbello <ipotapov101@gmail.com>
2024-02-20 18:32:37 +0400

    add a theme change

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="wrapper">
-
+      <HeaderItem/>
   </div>
 </template>
 
 <script setup lang="ts">
+import HeaderItem from './components/HeaderItem.vue'
+import { useStore } from './stores/store';
+const store = useStore()
 
 </script>
 
 <style scoped lang="scss">
 .wrapper {
-  background-color: var(--primary-black);
-  min-height: 100%;
+  background-color: v-bind('store.themeSettings.settings.background_color');
+  min-height: 100vh;
 }
 </style>
